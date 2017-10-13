5714777d063836422f2cabfacc30f7145e3f0a3d
Cory House <Cory.House@coxautoinc.com>
2017-10-13 13:33:41 -0700

    Added switch and redirect

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,20 @@ import UserPage from "./pages/UserPage";
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Switch,
+  Redirect
 } from 'react-router-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redirect: false
+    }
+  }
+
   render() {
     return (
       <Router>
@@ -18,10 +28,16 @@ class App extends React.Component {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/search">Search</Link></li>
           </ul>
+
+          {this.state.redirect && <Redirect from="/searchold" to="/search" /> }
+
+          <Switch>
+            <Route path="/" exact component={HomePage}/>
+            <Route path="/search" component={UserSearchPage}/>
+            <Route path="/user/:id" component={UserPage}/>
+            <Route component={HomePage}/>
+          </Switch>
           
-          <Route path="/" exact component={HomePage}/>
-          <Route path="/search" component={UserSearchPage}/>
-          <Route path="/user/:id" component={UserPage}/>
         </div>
       </Router>
     );
